5de80c3d954b57980578df77337d472560960799
Lee Vlad <leevladislove@gmail.com>
2023-10-03 22:55:08 +0600

    permissions

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,6 +7,7 @@ class Book(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     author = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'id {self.id}: {self.name}'
